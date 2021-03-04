ef1e1dfa0b1feb8664c913bbcd28b13b395306f6
gdedward <79942375+gdedward@users.noreply.github.com>
2021-03-04 14:46:25 -0500

    updated version
    Updated version

---
+++ MedTracker_System/MedTracker.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+import os
+print(os.getcwd())
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("MedTracker_Home.html")
+	# return "Hello world!" + os.getcwd()
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+	
+@app.route("/salvadore")
+def salvadore():
+	return "Hello, Salvadore"
+
+if __name__ == "__main__":
+    app.run(debug=True)
